40f29a52e905ef4ddede0ed1c796de60dd366173
Lucas <n1211392@my.ntu.ac.uk>
2025-08-20 21:07:45 +0100

    added polls and can be used on replys too

--- main.py
+++ main.py
@@ -29,40 +29,69 @@ async def on_message(message):
         #deletes the message
         await message.delete()
 
-        tweeterHandleAndText = (message.content.split('@', 1))
-        Handle1 = tweeterHandleAndText[0].replace('tweet', '', 1).strip()
-        Handle2AndText = tweeterHandleAndText[1].split('\n', 1)
-        Handle2 = Handle2AndText[0]
-        Text = Handle2AndText[1]
+        #splits the current text to handle before and after the @ and then the text
+        Handle1, Handle2, Text = await handleAndText(message.content, "tweet")
+        userMsg = await forReplys(Handle1, Handle2, message)
 
-        #checks if the message is a reply to another tweet
-        if message.reference:
-            #gets the person being replied toos message, to get handle to add to reply message
-            messageRep = ((await message.channel.fetch_message(message.reference.message_id)).content).split("\n")
-
-            #get first line and checks if reply goes to line 2 to get proper handle line
-            if "replying" in messageRep[0]:
-                handle = messageRep[1].split("@")
-                print(message.content)
-            else:
-                handle = messageRep[0].split("@")
-                print(message.content)
-
-            #formats users message
-            userMsg = (f"*replying to @{handle[1]}*\n**{Handle1}** @{Handle2}\n{Text}")
-        else:
-            userMsg = (f"**{Handle1}** @{Handle2}\n{Text}")
+        userMsg += f"\n{Text}"
         print(message.author.mention + "\n" + userMsg)
 
         #rather than send the message into the chat it instead sends the message to the persons dms
         #more uses with stuff such as tupper which is person specific and the bot cant send as a tupper
         await message.author.send(userMsg)
 
+
+    if  message.content.lower().lstrip().startswith("poll "):
+        #deletes the message
+        await message.delete()
+
+        #splits the current text to handle before and after the @ and then the text
+        Handle1, Handle2, Text = await handleAndText(message.content, "poll")
+        userMsg = await forReplys(Handle1, Handle2, message)
+        PollOptions = Text.splitlines()
+
+        userMsg += "\nnumber of responces: 0"
+        for option in PollOptions:
+            userMsg += f"\n> {option} 0%"
+
+        print(message.author.mention + "\n" + userMsg)
+
+        await message.author.send(userMsg)
+
     #lets the bot handle other messages while dealing with 1 message
     await bot.process_commands(message)
 
+
+async def handleAndText(MessageCont, type):
+    tweeterHandleAndText = (MessageCont.split('@', 1))
+    Handle1 = tweeterHandleAndText[0].replace(type, '', 1).strip()
+    Handle2AndText = tweeterHandleAndText[1].split('\n', 1)
+    Handle2 = Handle2AndText[0]
+    Text = Handle2AndText[1]
+    return (Handle1, Handle2, Text)
+
+async def forReplys(Handle1, Handle2, message):
+    #checks if the message is a reply to another tweet
+    if message.reference:
+        # gets the person being replied toos message, to get handle to add to reply message
+        messageRep = ((await message.channel.fetch_message(message.reference.message_id)).content).split("\n")
+
+        # get first line and checks if reply goes to line 2 to get proper handle line
+        if "replying" in messageRep[0]:
+            handle = messageRep[1].split("@")
+            print(message.content)
+        else:
+            handle = messageRep[0].split("@")
+            print(message.content)
+
+        # formats users message
+        userMsg = (f"*replying to @{handle[1]}*\n**{Handle1}** @{Handle2}")
+    else:
+        userMsg = (f"**{Handle1}** @{Handle2}")
+    return userMsg
+
 @bot.command()
 async def thelp(ctx):
-    await ctx.send(f"welcome to disctweet\n\nthe bot as it is in this state(version 1) has 2 functions:\ntweet and reply\n-tweet is done by writing the word 'tweet' followed by your handle (user @user) then your text for the tweet\n-reply is done by following the same format as the first one but by treating it like a normal discord reply\nthe message will be deleted from the channel and the tweet formated version in your dms\n\nbelow is a formated command:\ntweet user @user\nyour message here")
+    await ctx.send(f"welcome to disctweet\n\nthe bot as it is in this state(version 1.3) has 2 functions:\ntweet and reply\n-tweet is done by writing the word 'tweet' followed by your handle (user @user) then your text for the tweet\n-reply is done by following the same format as the first one but by treating it like a normal discord reply\nthe message will be deleted from the channel and the tweet formated version in your dms\n\nbelow is a formated command:\ntweet user @user\nyour message here\n3rd function in development although usable wont do anything which is poll")
 
 bot.run(token, log_handler=handler, log_level=logging.DEBUG)

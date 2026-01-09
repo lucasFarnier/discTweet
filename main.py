d19e1be386617ff89accaac160be063507212e70
lucas <lucasfarnier71@gmail.com>
2026-01-09 16:53:45 +0000

    changed if so if hes mentioned at all he responds

--- main.py
+++ main.py
@@ -126,7 +126,7 @@ async def on_message(message):
         await message.author.send(userMsg + "\n" + finaltTweetMsg)
 
 
-    if message.content.lower().lstrip().startswith("gorkus "):
+    if message.content.lower().lstrip().contains("gorkus"):
         exuse = await gorkus()
         print(exuse)
 

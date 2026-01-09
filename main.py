4696284d338985aecc4ec275a268cabb44f03de5
lucas <lucasfarnier71@gmail.com>
2026-01-09 16:57:28 +0000

    its search not contain apparently

--- main.py
+++ main.py
@@ -126,7 +126,7 @@ async def on_message(message):
         await message.author.send(userMsg + "\n" + finaltTweetMsg)
 
 
-    if message.content.lower().lstrip().contains("gorkus"):
+    if message.content.lower().lstrip().search("gorkus"):
         exuse = await gorkus()
         print(exuse)
 

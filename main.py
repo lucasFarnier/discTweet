1b2e5fed24320a50a5f2abb01cde447def961258
Lucas <n1211392@my.ntu.ac.uk>
2026-01-11 18:00:18 +0000

    new attempt at gorkus

--- main.py
+++ main.py
@@ -126,7 +126,7 @@ async def on_message(message):
         await message.author.send(userMsg + "\n" + finaltTweetMsg)
 
 
-    elif "gorkus " in message.content.lower().lstrip() and not(message.content.lower().lstrip().startswith("tweet ")) and not message.author.bot:
+    elif "gorkus " in message.content.lower().lstrip() and not(message.content.lower().lstrip().startswith("tweet ")):
         exuse = await gorkus()
         print(exuse)
 

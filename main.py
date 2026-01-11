3d8fa03375a7b408389486eeb942e2d3ab2d93cd
Lucas <n1211392@my.ntu.ac.uk>
2026-01-11 19:38:04 +0000

    testing space format on tweet atmpt 2

--- main.py
+++ main.py
@@ -164,7 +164,7 @@ async def handleAndText(MessageCont, type):
     Handle2 = Handle2AndText[0]
 
     Text = Handle2AndText[1]
-    if "\n" not in (Text.split('\n', 1))[0]:
+    if "" not in (Text.split('\n', 1))[0]:
         print("no space")
     return (Handle1, Handle2, Text)
 
